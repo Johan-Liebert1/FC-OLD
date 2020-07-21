30f0450fd48daee1e853146e8d74fb1a39a88e9f
Johan-Liebert1 <pragyanpoudyal41999@gmail.com>
2020-07-21 22:24:01 +0530

    Add bootstrap, style form, add separators

--- src/AddQuestionForm.js
+++ src/AddQuestionForm.js
@@ -1,53 +1,83 @@
-import React from 'react'
-import {v4 as uuid} from 'uuid'
+import React, {useState} from 'react'
 import {Link} from 'react-router-dom'
 import useInputState from './hooks/useInputState'
 
-import { makeStyles, withStyles } from '@material-ui/core/styles';
+import { withStyles } from '@material-ui/core/styles';
 import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
 
-const useStyles = makeStyles((theme) => ({
+const useStyles = {
   root: {
-        width: '50%',
+        width: '75%',
         display: 'flex',
         flexDirection: 'column',
+        margin: '100px auto',
     },
-
-}))
+    separatorField: {
+        // height: '15px'
+    }
+}
 
 function AddQuestionForm({classes, data}) {
-    const [value, handleChange, reset] = useInputState("")
+    const [separator, handleSeperatorChange, resetSeparator] = useInputState("")
+    const [value, handleValueChange, reset] = useInputState("")
 
     const submitForm = (event) => {
         event.preventDefault()
+        let separatedBy = separator
         let text = value
-        let all_things = text.split("\n") 
+        let all_things = text.split("\n")
+        console.log(all_things) 
         var new_data = data
         for (let i = 0 ; i < all_things.length; i++){
-            let question = all_things[i].split("-")[0]
-            let answer = all_things[i].split("-")[1]
+            let question = all_things[i].split(separatedBy)[0]
+            let answer = all_things[i].split(separatedBy)[1]
             let id = new_data.length
             new_data.push({id: id, question: question, answer: answer})
         }
         window.localStorage.setItem("words", JSON.stringify(new_data))
         
         reset()
+        resetSeparator()
     }
     return (
-        <div  className={classes.root}>
+        <div className={classes.root}>
             <form onSubmit={submitForm}>
-                <TextField
-                    id="standard-multiline-flexible"
-                    label="Multiline"
-                    multiline
-                    rowsMax={10}
-                    value={value}
-                    onChange={handleChange}
-                    style={{width: '50%'}}
-                />
-                <button type="submit">Submit</button>
+                <div className="form-group mb-5">
+                    <TextField
+                        id="standard-multiline-flexible"
+                        label="Questions and Answers Seperated by a Symbol"
+                        multiline
+                        variant="outlined"
+                        rows={10}
+                        value={value}
+                        onChange={handleValueChange}
+                        fullWidth
+                    />
+                </div>
+
+                <div className="form-group mb-5">
+                    <p>
+                        Please enter the character seperating your questions from answers
+                    </p>
+                    <TextField 
+                        className={classes.separatorField}
+                        id="outlined-basic" 
+                        label="Seperator" 
+                        variant="outlined" 
+                        value={separator}
+                        onChange={handleSeperatorChange}
+                    />
+                </div>
+                
+                
+                <div className="form-group mb-5">
+                    <Button variant="outlined" color="primary" type="submit">
+                        Submit
+                    </Button>
+                </div>
             </form>
-            <Link to="/">Home</Link>
+            <Link to="/"><button>Home</button></Link>
         </div>
     )
 }
