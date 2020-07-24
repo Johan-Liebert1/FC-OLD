eeb1bc48375c7b7d6fa8eed95356946b0bae434f
Johan-Liebert1 <pragyanpoudyal41999@gmail.com>
2020-07-24 17:22:42 +0530

    Updates

---
+++ precache-manifest.2e3460fcb3de469aa7cca6f170190723.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5db9e9c7858a5a937f960cd51e85f6dc",
+    "url": "/flash_cards/index.html"
+  },
+  {
+    "revision": "d7ef677d6d41c1b99a21",
+    "url": "/flash_cards/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "a2d2b793115f11380b10",
+    "url": "/flash_cards/static/css/main.1cae9c9e.chunk.css"
+  },
+  {
+    "revision": "d7ef677d6d41c1b99a21",
+    "url": "/flash_cards/static/js/2.c14ab4ae.chunk.js"
+  },
+  {
+    "revision": "29d944d350ae16cdb652b452ef3b2793",
+    "url": "/flash_cards/static/js/2.c14ab4ae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a2d2b793115f11380b10",
+    "url": "/flash_cards/static/js/main.df6fdd2c.chunk.js"
+  },
+  {
+    "revision": "b973e2fbada5cd4dd258",
+    "url": "/flash_cards/static/js/runtime-main.886551fc.js"
+  }
+]);
